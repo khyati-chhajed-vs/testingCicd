2b976deba8f1e9cc0d40a8e0d802118f202dda08
gaurav-sharma-vs <khyati.chhajed@vectoscalar.com>
2024-08-28 15:46:04 +0530

    test

--- scripts/build.py
+++ scripts/build.py
@@ -25,10 +25,10 @@ print("Generating Build...")
 os.system("npm run build")
 
 print(f"Copying 'eb/package.json' Files Into '{dist_dir}' Directory.")
-shutil.copy("./eb/package.json", dist_dir)
+shutil.copy("../eb/package.json", dist_dir)
 
 print(f"Copying 'eb/package-lock.json' Files Into '{dist_dir}' Directory.")
-shutil.copy("./eb/package-lock.json", dist_dir)
+shutil.copy("../eb/package-lock.json", dist_dir)
 
 os.chdir(dist_dir)
 
